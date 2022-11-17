cafaf3f711937b9899ce5474e85d111cc5f9a321
Rerumu <25379555+Rerumu@users.noreply.github.com>
2022-11-17 10:36:18 -0500

    Add read write annotation pass

--- wasm-ir/src/passes/mod.rs
+++ wasm-ir/src/passes/mod.rs
@@ -1,1 +1,2 @@
 pub mod dead_code_elimination;
+pub mod read_write_annotation;
---
+++ wasm-ir/src/passes/read_write_annotation.rs
@@ -0,0 +1,155 @@
+use std::collections::{BTreeMap, BTreeSet};
+
+use wasmparser::Operator;
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+pub enum Var {
+	Local(u32),
+	Global(u32),
+}
+
+#[derive(Default, Clone)]
+pub struct ReadWriteLabel {
+	read_set: BTreeSet<Var>,
+	write_set: BTreeSet<Var>,
+}
+
+impl ReadWriteLabel {
+	pub fn read_set(&self) -> &BTreeSet<Var> {
+		&self.read_set
+	}
+
+	pub fn clear(&mut self) {
+		self.read_set.clear();
+		self.write_set.clear();
+	}
+
+	pub fn read_extend(&mut self, other: &BTreeSet<Var>) {
+		self.read_set.extend(other);
+	}
+
+	pub fn linear_merge(&mut self, other: &Self) {
+		self.read_set.retain(|v| !other.write_set.contains(v));
+		self.read_set.extend(&other.read_set);
+		self.write_set.extend(&other.write_set);
+	}
+
+	fn branch_merge(&mut self, other: &Self) {
+		self.read_set.extend(&other.read_set);
+		self.write_set.retain(|v| other.write_set.contains(v));
+	}
+}
+
+#[derive(Default)]
+pub struct ReadWriteAnnotation {
+	branch_stack: Vec<Option<usize>>,
+	pending_stack: Vec<ReadWriteLabel>,
+
+	result_map: BTreeMap<usize, ReadWriteLabel>,
+	label_scratch: ReadWriteLabel,
+}
+
+impl ReadWriteAnnotation {
+	fn handle_block(&mut self, key: usize) {
+		let popped = self.pending_stack.pop().unwrap();
+
+		self.branch_stack.pop().unwrap();
+		self.result_map.insert(key, popped);
+	}
+
+	fn handle_if(&mut self, key: usize) {
+		let mut popped = self.pending_stack.pop().unwrap();
+
+		self.result_map.insert(key + 1, popped.clone());
+
+		if let Some(other) = self.branch_stack.pop().unwrap() {
+			popped.branch_merge(&self.result_map[&other]);
+		}
+
+		self.result_map.insert(key, popped);
+	}
+
+	fn handle_else(&mut self, key: usize) {
+		let popped = self.pending_stack.pop().unwrap();
+
+		*self.branch_stack.last_mut().unwrap() = Some(key);
+
+		self.result_map.insert(key + 1, popped);
+	}
+
+	fn handle_end(&mut self) {
+		self.branch_stack.push(None);
+		self.pending_stack.push(ReadWriteLabel::default());
+	}
+
+	fn handle_boundary(&mut self, key: usize, inst: &Operator) -> bool {
+		match inst {
+			Operator::Block { .. } | Operator::Loop { .. } => self.handle_block(key),
+			Operator::If { .. } => self.handle_if(key),
+			Operator::Else => self.handle_else(key),
+			Operator::End => self.handle_end(),
+			_ => return false,
+		}
+
+		true
+	}
+
+	fn track_operation(&mut self, inst: &Operator) {
+		let read_set = &mut self.label_scratch.read_set;
+		let write_set = &mut self.label_scratch.write_set;
+
+		match inst {
+			Operator::LocalGet { local_index } => {
+				read_set.insert(Var::Local(*local_index));
+			}
+			Operator::LocalSet { local_index } => {
+				write_set.insert(Var::Local(*local_index));
+			}
+			Operator::LocalTee { local_index } => {
+				read_set.insert(Var::Local(*local_index));
+				write_set.insert(Var::Local(*local_index));
+			}
+			Operator::GlobalGet { global_index } => {
+				read_set.insert(Var::Global(*global_index));
+			}
+			Operator::GlobalSet { global_index } => {
+				write_set.insert(Var::Global(*global_index));
+			}
+			_ => {}
+		}
+	}
+
+	fn add_label_data(&mut self, code: &[Operator]) {
+		for (i, inst) in code.iter().enumerate().rev() {
+			if self.handle_boundary(i, inst) {
+				continue;
+			}
+
+			self.label_scratch.clear();
+
+			self.track_operation(inst);
+
+			self.pending_stack
+				.last_mut()
+				.unwrap()
+				.linear_merge(&self.label_scratch);
+		}
+	}
+
+	fn add_last_label(&mut self) {
+		let last = self.pending_stack.pop().unwrap();
+
+		self.result_map.insert(usize::MAX, last);
+	}
+
+	pub fn run(&mut self, code: &[Operator]) -> BTreeMap<usize, ReadWriteLabel> {
+		self.branch_stack.clear();
+		self.pending_stack.clear();
+		self.label_scratch.clear();
+
+		self.add_label_data(code);
+		self.add_last_label();
+
+		std::mem::take(&mut self.result_map)
+	}
+}
